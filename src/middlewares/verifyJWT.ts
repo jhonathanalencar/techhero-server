f0d27cc72bf1c3382acb9d2b7ffe77592e7ab63d
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2023-01-11 20:18:40 -0300

    fix: prevent verifyJWT middleware from stopping the server

--- src/middlewares/verifyJWT.ts
+++ src/middlewares/verifyJWT.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-import { UnauthenticatedError, UnauthorizedError } from '@/errors';
 import { UserRoles } from '@/models';
 
 interface JwtPayload {
@@ -16,14 +15,14 @@ function verifyJWT(request: Request, response: Response, next: NextFunction) {
   const authHeader = request.headers.authorization;
 
   if (!(authHeader?.startsWith('Bearer ') ?? false)) {
-    throw new UnauthenticatedError('Unauthorized');
+    return response.status(401).json({ error: 'Unauthorized' });
   }
 
   const token = authHeader?.split(' ')[1] as string;
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
     if (err !== null) {
-      throw new UnauthorizedError('Forbidden');
+      return response.status(403).json({ error: 'Forbidden' });
     }
 
     request.user = (decoded as JwtPayload).userInfo;
